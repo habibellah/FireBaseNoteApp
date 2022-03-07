81ae493f0057adeecf1e2ca1f82be176654a1cfe
habibellah <habibayata1@gmail.com>
2022-03-07 12:06:32 +0100

    new changes in ui

--- app/src/main/java/habibellah/ayata/firenote/MainActivity.java
+++ app/src/main/java/habibellah/ayata/firenote/MainActivity.java
@@ -3,10 +3,7 @@ package habibellah.ayata.firenote;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import habibellah.ayata.firenote.databinding.ActivityMainBinding;
 
@@ -17,14 +14,12 @@ ActivityMainBinding binding ;
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater(),null,false);
         setContentView(binding.getRoot());
-binding.addNote.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View view) {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);          View view1 =   getLayoutInflater().inflate(R.layout.alert_note,null);
-          builder.setView(view1);
-      AlertDialog alertDialog = builder.create();
-      alertDialog.show();
-    }
+binding.addNote.setOnClickListener(view -> {
+    final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+    View view1 =   getLayoutInflater().inflate(R.layout.alert_note,null);
+      builder.setView(view1);
+  AlertDialog alertDialog = builder.create();
+  alertDialog.show();
 });
 
 
